914b1846e659b4c39b2c51efb5eccb60bb4e4ee5
Trey Hayden <treyhay@gmail.com>
2022-07-06 22:27:09 -0400

    Create handler.py for cron&api functions

---
+++ handler.py
@@ -0,0 +1,21 @@
+import datetime
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def getMetrics(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def run(event, context):
+    current_time = datetime.datetime.now().time()
+    name = context.function_name
+    logger.info("Your cron function " + name + " ran at " + str(current_time))
